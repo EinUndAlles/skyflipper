ce92cf84f340d5e82136cc24b617c21e7868dc86
EinUndAlles <127785125+EinUndAlles@users.noreply.github.com>
2026-03-17 11:57:19 -0700

    chore: add nbt lookup uniqueness migration

---
+++ Migrations/20260317185444_AddUniqueIndexToNbtLookup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkyFlipperSolo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexToNbtLookup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_NBTLookups_AuctionId_KeyId",
+                table: "NBTLookups",
+                columns: new[] { "AuctionId", "KeyId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_NBTLookups_AuctionId_KeyId",
+                table: "NBTLookups");
+        }
+    }
+}
